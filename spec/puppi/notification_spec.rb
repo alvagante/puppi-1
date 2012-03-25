c16d718e4ae43fa45c2cb01b5bfe0c69eaaf4bd3
Celso Fernandes <fernandes@zertico.com>
2012-03-25 02:38:09 -0300

    test to ensure notification stdout is used when no method is specified

--- spec/puppi/notification_spec.rb
+++ spec/puppi/notification_spec.rb
@@ -10,6 +10,10 @@ describe "Puppi::Notification" do
   it "should show raise exception when a unknown method is specified" do
     expect { Puppi::Notification.new(['unknown'], 'output') }.to raise_error(RuntimeError)
   end
+  
+  it "should use stdout notification method when no one is specified" do
+    Puppi::Notification.new([], 'output').notify_methods.should include('stdout')
+  end
 
   it "should use stdout notification method when specified" do
     Puppi::Notification.new(['stdout'], 'output').notify_methods.should include('stdout')
@@ -17,5 +21,11 @@ describe "Puppi::Notification" do
     
   it "should use mail notification method when specified" do
     Puppi::Notification.new(['mail'], 'output').notify_methods.should include('mail')
+  end
+  
+  it "should use mail and stdout notification methods when both are specified" do
+    @notifications_used = Puppi::Notification.new(['mail', 'stdout'], 'output')
+    @notifications_used.notify_methods.should include('mail')
+    @notifications_used.notify_methods.should include('stdout')
   end
 end
